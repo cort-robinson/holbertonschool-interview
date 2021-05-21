e177e39f0a2eddb44d849edb13d98c1fc614e568
Cort Robinson <cortrobinson99@gmail.com>
2021-05-20 21:36:42 -0500

    add files

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles.
+ * @grid1: The first grid.
+ * @grid2: The second grid.
+ * Return: The sum of grid1 and grid2.
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+
+}
