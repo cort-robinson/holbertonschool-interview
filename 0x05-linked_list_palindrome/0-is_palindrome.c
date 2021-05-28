df8332cfc66221629bd487e18092be3ba4baf4a4
Cort Robinson <cortrobinson99@gmail.com>
2021-05-27 23:24:30 -0500

    update files with completed code

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -8,16 +8,49 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *idx = *head;
+	listint_t *fast = *head;
+	listint_t *slow = *head;
+	listint_t *first_half, *second_half;
 
 	if (*head == NULL)
-		return 1;
+		return (1);
 
-	while ((idx)->next != NULL)
+	while (fast->next != NULL && fast->next->next != NULL)
 	{
-		putchar(idx->n);
-		idx = idx->next;
+		fast = fast->next->next;
+		slow = slow->next;
 	}
 
-	return 0;
+	second_half = reverse(slow->next);
+	first_half = *head;
+
+	while (second_half != NULL && first_half != NULL)
+	{
+		if (first_half->n != second_half->n)
+			return (0);
+		second_half = second_half->next;
+		first_half = first_half->next;
+	}
+	return (1);
+}
+
+/**
+ * reverse - Reverses linked list.
+ * @head: Head of list to reverse.
+ * Return: Reversed list head.
+ */
+
+listint_t *reverse(listint_t *head)
+{
+	listint_t *new = NULL;
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		head->next = new;
+		new = head;
+		head = next;
+	}
+	return (new);
 }
