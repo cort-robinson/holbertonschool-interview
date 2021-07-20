840f35f3335cc01f8bb217b0961fa4cb8fb66145
Cort Robinson <cortrobinson99@gmail.com>
2021-07-20 16:50:32 -0500

    update formatting

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 The N queens puzzle is the challenge of placing N
-non-attacking queens on an N×N chessboard. Write a
+non-attacking queens on an N×N board. Write a
 program that solves the N queens problem.
 
 Usage: nqueens N
@@ -23,61 +23,59 @@ import sys
 boardcnt = 0
 
 
-def IsBoardOk(chessboard, row, col):
-    # Check if there is a queen 'Q' positioned to the left of column col on the same row.
+def IsBoardOk(board, row, col):
+    """Check if current queen placements is okay"""
     for c in range(col):
-        if (chessboard[row][c] == 'Q'):
+        if (board[row][c] == 'Q'):
             return False
-
-    # Check if there is queen 'Q' positioned on the upper left diagonal
     for r, c in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
-        if (chessboard[r][c] == 'Q'):
+        if (board[r][c] == 'Q'):
             return False
-
-    # Check if there is queen 'Q' positioned on the lower left diagonal
-    for r, c in zip(range(row+1, len(chessboard), 1), range(col-1, -1, -1)):
-        if (chessboard[r][c] == 'Q'):
+    for r, c in zip(range(row+1, len(board), 1), range(col-1, -1, -1)):
+        if (board[r][c] == 'Q'):
             return False
-
     return True
 
 
-def DisplayBoard(chessboard):
+def DisplayBoard(board):
+    """print current queen placements"""
     positions = []
-    for row in range(len(chessboard)):
-        for tile in range(len(chessboard[row])):
-            if chessboard[row][tile] == 'Q':
+    for row in range(len(board)):
+        for tile in range(len(board[row])):
+            if board[row][tile] == 'Q':
                 positions.append([row, tile])
     print(positions)
 
 
-def PlaceNQueens(chessboard, col):
-    # If all the columns have a queen 'Q', a solution has been found.
+def PlaceNQueens(board, col):
+    """place N queens on the board"""
     global boardcnt
-    if (col >= len(chessboard)):
+    if (col >= len(board)):
         boardcnt += 1
-        DisplayBoard(chessboard)
+        DisplayBoard(board)
     else:
-        # Else try placing the queen on each row of the column and check if the chessboard remains OK.
-        for row in range(len(chessboard)):
-            chessboard[row][col] = 'Q'
-            if (IsBoardOk(chessboard, row, col) == True):
-                # Chess board was OK, hence try placing the queen 'Q' in the next column.
-                PlaceNQueens(chessboard, col + 1)
-            # As previously placed queen was not valid, restore '.'
-            chessboard[row][col] = '.'
+        for row in range(len(board)):
+            board[row][col] = 'Q'
+            if (IsBoardOk(board, row, col)):
+                PlaceNQueens(board, col + 1)
+            board[row][col] = '.'
 
 
 def main():
-    chessboard = []
-    N = int(sys.argv[1])
-
+    """Main"""
+    board = []
+    try:
+        N = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        exit()
+    if N < 4:
+        print('N must be at least 4')
+        exit()
     for i in range(N):
         row = ["."] * N
-        chessboard.append(row)
-
-    # Start placing the queen 'Q' from the 0'th column.
-    PlaceNQueens(chessboard, 0)
+        board.append(row)
+    PlaceNQueens(board, 0)
 
 
 if __name__ == "__main__":
