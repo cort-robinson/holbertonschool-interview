72ea8a3009cf20b9d945c07a60665d6921752e1f
Cort Robinson <cortrobinson99@gmail.com>
2021-08-12 05:56:14 -0500

    semistandard compliance

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,30 +1,28 @@
 #!/usr/bin/node
-request = require('request');
+const request = require('request');
 
-
-function getCharacter(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                resolve(body)
-            }
-            else {
-                reject(error)
-            }
-        });
-    })
+function getCharacter (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
+  });
 }
 
-async function printName(url) {
-    const body = await getCharacter(url);
-    console.log(JSON.parse(body).name);
+async function printName (url) {
+  const body = await getCharacter(url);
+  console.log(JSON.parse(body).name);
 }
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        const characterUrls = JSON.parse(body).characters;
-        for (url of characterUrls) {
-            await printName(url);
-        }
-    };
+  if (!error && response.statusCode === 200) {
+    const characterUrls = JSON.parse(body).characters;
+    for (const url of characterUrls) {
+      await printName(url);
+    }
+  }
 });
