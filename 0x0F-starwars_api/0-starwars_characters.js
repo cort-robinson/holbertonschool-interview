0c2f7a53b758aa597b143924040af1e74a87d154
Cort Robinson <cortrobinson99@gmail.com>
2021-08-12 05:53:29 -0500

    update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,15 +2,29 @@
 request = require('request');
 
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+function getCharacter(url) {
+    return new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                resolve(body)
+            }
+            else {
+                reject(error)
+            }
+        });
+    })
+}
+
+async function printName(url) {
+    const body = await getCharacter(url);
+    console.log(JSON.parse(body).name);
+}
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        characterUrls = JSON.parse(body).characters;
+        const characterUrls = JSON.parse(body).characters;
         for (url of characterUrls) {
-            request(url, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(JSON.parse(body).name);
-                }
-            });
+            await printName(url);
         }
     };
 });
