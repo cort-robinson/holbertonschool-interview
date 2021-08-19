353baeceb015706549e0d108c069b46a5d908e12
Cort Robinson <cortrobinson99@gmail.com>
2021-08-19 07:11:01 -0500

    remove type hints

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -3,25 +3,25 @@
 """
 
 
-def find_gap(start_index: int, walls: list) -> tuple:
+def find_gap(start_index, walls):
     """Measure horizontal gap between current wall and next wall
     Return tuple (gap size, size of shorter wall), or none if no next wall
     """
-    gap: int = 0
+    gap = 0
     for col in range(start_index + 1, len(walls)):
         if walls[col] > 0:
             return (gap, min(walls[start_index], walls[col]))
         gap += 1
 
 
-def rain(walls: list) -> int:
+def rain(walls):
     """Given a list of non-negative integers representing the heights of walls
     with unit width 1, as if viewing the cross-section of a relief map,
     calculate how many square units of water will be retained after it rains.
     """
     if len(walls) == 0:
         return 0
-    rainwater: int = 0
+    rainwater = 0
     for i in range(len(walls) - 1):
         if walls[i] > walls[i + 1]:
             gap_info = find_gap(i, walls)
