7fa03275aeaae2e0a728124c754cec248f5c638d
Cort Robinson <cortrobinson99@gmail.com>
2021-08-19 06:30:06 -0500

    update 0-rain.py

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,22 +4,14 @@
 
 
 def rain(walls) -> int:
-    """
-    Given a list of non-negative integers representing the heights of walls
+    """Given a list of non-negative integers representing the heights of walls
     with unit width 1, as if viewing the cross-section of a relief map,
     calculate how many square units of water will be retained after it rains.
-
-    * Prototype: def rain(walls)
-    * walls is a list of non-negative integers.
-    * Return: Integer indicating total amount of rainwater retained.
-    * Assume that the ends of the list (before index 0 and after index
-      walls[-1]) are not walls, meaning they will not retain water.
-    * If the list is empty return 0.
     """
     if len(walls) == 0:
         return 0
     rainwater = 0
-    for i in range(len(walls)-2):
+    for i in range(len(walls)-1):
         if walls[i] > walls[i+1]:
             rainwater += (walls[i] - walls[i+1])
     return rainwater
