069558223c46cbb3dd8c4b4a25a80123af81210f
Cort Robinson <cortrobinson99@gmail.com>
2021-08-19 07:08:54 -0500

    update 0-rain.py

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -3,15 +3,31 @@
 """
 
 
-def rain(walls) -> int:
+def find_gap(start_index: int, walls: list) -> tuple:
+    """Measure horizontal gap between current wall and next wall
+    Return tuple (gap size, size of shorter wall), or none if no next wall
+    """
+    gap: int = 0
+    for col in range(start_index + 1, len(walls)):
+        if walls[col] > 0:
+            return (gap, min(walls[start_index], walls[col]))
+        gap += 1
+
+
+def rain(walls: list) -> int:
     """Given a list of non-negative integers representing the heights of walls
     with unit width 1, as if viewing the cross-section of a relief map,
     calculate how many square units of water will be retained after it rains.
     """
     if len(walls) == 0:
         return 0
-    rainwater = 0
-    for i in range(len(walls)-1):
-        if walls[i] > walls[i+1]:
-            rainwater += (walls[i] - walls[i+1])
+    rainwater: int = 0
+    for i in range(len(walls) - 1):
+        if walls[i] > walls[i + 1]:
+            gap_info = find_gap(i, walls)
+            if not gap_info:
+                break
+            gap = gap_info[0]
+            small_wall = gap_info[1]
+            rainwater += (small_wall * gap)
     return rainwater
