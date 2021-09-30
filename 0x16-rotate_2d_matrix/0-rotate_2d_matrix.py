a6360d2f6529aeec415cef1a81b56f5678e43086
Cort Robinson <cortrobinson99@gmail.com>
2021-09-30 18:33:43 -0500

    add task 0 file

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module defines rotate_2d_matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates a n x n 2D matrix 90 degrees clockwise"""
+    n = len(matrix)
+    for layer in range(n // 2):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            offset = i - first
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset] = matrix[i][last]
+            matrix[i][last] = top
