7da3cac840c685a79e8bb9a809142177f4637e6c
Cort Robinson <cortrobinson99@gmail.com>
2021-10-14 18:36:22 -0500

    update primary task file

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,5 +1,66 @@
 #include "sort.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * merge - merge two sorted arrays
+ *
+ * @array: array to merge
+ * @size: size of array
+ * @middle: middle index
+ * @temp: temporary array
+ */
+void merge(int *array, size_t size, size_t middle, int *temp)
+{
+    size_t i = 0, j = middle, k = 0;
+
+    printf("Merging...\n[left]: ");
+    print_array(array, middle);
+    printf("[right]: ");
+    print_array(array + middle, size - middle);
+    while (k < size)
+    {
+        if (j == size)
+        {
+            temp[k] = array[i];
+            i++;
+        }
+        else if (i == middle || array[j] < array[i])
+        {
+            temp[k] = array[j];
+            j++;
+        }
+        else
+        {
+            temp[k] = array[i];
+            i++;
+        }
+        k++;
+    }
+    for (i = 0; i < size; i++)
+        array[i] = temp[i];
+    printf("[Done]: ");
+    print_array(temp, size);
+}
+
+/**
+ * merge_sort_rec - sorts an array of integers in ascending order using
+ * the Merge sort algorithm recursively
+ *
+ * @array: array of integers
+ * @size: size of the array
+ * @temp: pointer to temp node
+ */
+void merge_sort_rec(int *array, size_t size, int *temp)
+{
+    if (size > 1)
+	{
+		merge_sort_rec(array, size / 2, temp);
+		merge_sort_rec(array + size / 2, size - size / 2, temp);
+		merge(array, size, size / 2, temp);
+	}
+}
+
 
 /**
  * merge_sort - Sorts an array of integers in ascending
@@ -12,37 +73,12 @@
 void merge_sort(int *array, size_t size)
 {
 	int *temp;
-	size_t i, j, k;
 
-	if (size < 2)
-		return;
-
-	temp = malloc(sizeof(int) * size);
-	if (temp == NULL)
-		return;
-
-	for (i = 0; i < size; i++)
-		temp[i] = array[i];
-
-	for (i = 1; i < size; i *= 2)
-	{
-		for (j = 0; j < size - i; j += i * 2)
-		{
-			for (k = 0; k < i; k++)
-			{
-				if (temp[j + k] > temp[j + i + k])
-				{
-					array[j + k] = temp[j + k];
-					array[j + i + k] = temp[j + i + k];
-				}
-				else
-				{
-					array[j + k] = temp[j + i + k];
-					array[j + i + k] = temp[j + k];
-				}
-			}
-		}
-	}
-	free(temp);
-	print_array(array, size);
+    if (!array || size < 2)
+        return;
+    temp = malloc(sizeof(int) * size);
+    if (!temp)
+        return;
+    merge_sort_rec(array, size, temp);
+    free(temp);
 }
