fa4accaab59afa5b0da4dc4e1b0cef9084264c28
cort-robinson <cortrobinson99@gmail.com>
2022-01-18 23:37:34 -0600

    change return of 'none' to None

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -5,7 +5,7 @@
 def isWinner(x, nums):
     """Determines winner of prime number game"""
     if x < 1 or len(nums) < 1:
-        return "none"
+        return None
     number = 0
     for i in range(len(nums)):
         number ^= nums[i % len(nums)]
