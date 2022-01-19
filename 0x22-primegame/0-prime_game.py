97fe2839edb8f684d515965b5ee7114d6d1af85c
cort-robinson <cortrobinson99@gmail.com>
2022-01-18 23:36:08 -0600

    fix for no winner edge cases

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -4,8 +4,12 @@
 
 def isWinner(x, nums):
     """Determines winner of prime number game"""
+    if x < 1 or len(nums) < 1:
+        return "none"
     number = 0
     for i in range(len(nums)):
         number ^= nums[i % len(nums)]
+
+    print(number)
 
     return "Ben" if number > 0 else "Maria"
